49b70707c06eeef3fc3e3eb7a2c87acd5d1c58ea
Kyle Scagnelli <kjs5107@rit.edu>
2019-02-08 13:39:20 -0500

    Created a service to show very basic flow between component and service. Renamed some files to make it a bit easier to look at. (.html files will have the prefix .template and .scss files will have the prefix .style instead of the default prefix .component for both of these file types).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AppService } from "./app.service"
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.template.html',
+  styleUrls: ['./app.style.scss']
 })
 export class AppComponent {
+
   title = 'syllabuddy';
+
+  constructor(private appService: AppService) { }
+
+  ngOnInit() {
+    this.helloWorld()
+  }
+
+  helloWorld() {
+    console.log(this.appService.helloWorld());
+  }
+
 }
