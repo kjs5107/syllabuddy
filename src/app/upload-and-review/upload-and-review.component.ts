7dcad21b811dc7a1f8a1f8feca10f34172f9aa11
Kyle Scagnelli <kjs5107@rit.edu>
2019-02-15 13:26:02 -0500

    Upload and Review Component: 1. Added Tesseract into the project and am successfully extracting the text of images 2. Added a gorgeous regex expression to extract all date formats I could muster up from the text 3. Converting raw dates into moment.js dates. Index.html: 1. Added CDN to import Tesseract (we'd have to setup more things to do it locally according to their docs, so just going for the easier solution of CDN).

--- src/app/upload-and-review/upload-and-review.component.ts
+++ src/app/upload-and-review/upload-and-review.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadAndReviewService } from "./upload-and-review.service";
+import * as moment from 'moment';
+declare let Tesseract: any;
 
 @Component({
   selector: 'app',
@@ -11,6 +13,39 @@ export class UploadAndReviewComponent implements OnInit {
   constructor(private upLoadAndReviewSerivce: UploadAndReviewService) { }
 
   ngOnInit() {
+
+    /**
+     * I found some base regex online and played around with it some more to suit our needs
+     * It will match all the formats I can think of... 2 2 4, 4 2 2, 2 4, 4 2, and 2 2.
+     * Where a digit above represents how many digits there are in the string and a space represents a space, /, -, or .
+     * For example: 10/10/2010 is 2 2 4.
+     *
+     * @type {RegExp}
+     */
+    let dateRegex:RegExp = /(\d{4}([.\-/ ])\d{2}\2\d{2}|\d{2}([.\-/ ])\d{2}\3\d{4}|\d{2}([.\-/ ])\d{4}|\d{4}([.\-/ ])\d{2}|\d{2}([.\-/ ])\d{2})/g;
+
+    (Tesseract as any).recognize('../../assets/images/testSyllabus.jpg')
+      .catch(err => console.error(err))
+      .then((result) => {
+
+        let allRawDates = result.text.match(dateRegex);
+
+        console.log(result);
+        console.log(allRawDates);
+
+        // TODO: If rawDates don't have three groups, we need to add the group to make it valid for Moment.js
+
+
+        // Convert rawDates to moment dates
+        let allDates = [];
+        for (let rawDate in allRawDates) {
+          let date = moment(rawDate).format('MM-DD-YYYY');
+          allDates.push(date);
+        }
+        console.log(allDates);
+
+      })
+      .finally(resultOrError => console.log(resultOrError))
 
   }
 
