b60a08e5344f85efb14a737863c297a197a43b1b
Nathan Ortega <ndo2439@rit.edu>
2019-03-19 11:01:03 -0400

    1.) Broke our code into other functions

--- src/app/upload-and-review/upload-and-review.component.ts
+++ src/app/upload-and-review/upload-and-review.component.ts
@@ -26,19 +26,15 @@ const keywords = [
 })
 export class UploadAndReviewComponent implements OnInit {
 
-
   @ViewChild('myPond') myPond: any;
 
   defaultState = { status: 'Upload a file to begin', progress: 0};
   recognitionState: { status: string, progress: number } = this.defaultState;
 
-  pondFiles = [
-
-  ];
-
   calendarEvents: {}[] = [];
   assignmentEvents: {}[] = [];
   assignmentsToReview: {}[] = [];
+
   assignmentTableData: any;
   displayAssignmentTable = false;
   examEvents: {}[] = [];
@@ -47,10 +43,13 @@ export class UploadAndReviewComponent implements OnInit {
   displayExamTable = false;
   tableColumns: string[] = ['Title', 'Date', 'Confidence'];
 
+
+  seenFiles = {};
+  pondFiles = [];
   pondOptions = {
     url: 'test',
     class: 'my-filepond',
-    multiple: false,
+    multiple: true,
     labelIdle: 'Drop files here',
     acceptedFileTypes: 'image/jpeg, image/png'
   };
@@ -62,7 +61,14 @@ export class UploadAndReviewComponent implements OnInit {
   pondHandleAddFile(event: any) {
     // extract the File object from the upload event
     const file = event.file.file;
-    this.extractDates(file);
+
+    // check if we've already seen this file
+    if (!this.seenFiles[file.name]) {
+      this.processFile(file);
+    }
+
+    // no matter what store it in the dictionary
+    this.seenFiles[file.name] = file;
   }
 
   pondHandleRemoveFile(event: any) {
@@ -89,124 +95,76 @@ export class UploadAndReviewComponent implements OnInit {
   }
 
 
-  /**
-   * For now this function's job is to extract dates from the passed in image
-   *
-   * @param f - A file object
-   */
-  extractDates(f) {
+  processFile( file ) {
 
-    /**
-     * I found some base regex online and played around with it some more to suit our needs
-     * It will match all the number based formats I can think of ... 2 2 4, 4 2 2, 2 4, 4 2, and 2 2.
-     * Where a digit above represents how many digits there are in the string and a space represents a space, /, -, or .
-     * For example: 10/10/2010 is 2 2 4.
-     *
-     * @type {RegExp}
-     */
-    let dateRegex: RegExp = /(\d{4}([.\-/ ])\d{2}\2\d{2}|\d{2}([.\-/ ])\d{2}\3\d{4}|\d{2}([.\-/ ])\d{4}|\d{4}([.\-/ ])\d{2}|\d{2}([.\-/ ])\d{2})/g;
-    // TODO: Need to add support for string based formats as well...e.g. Fri Feb 15...
+      // OCR the file
+      Tesseract.recognize(file)
+        .progress((message) => this.recognitionState = message)
+        .catch(err => console.error(err))
+        .then((result) => {
 
-    Tesseract.recognize(f)
-      .progress((message) => this.recognitionState = message )
-      .catch(err => console.error(err))
-      .then((result) => {
+          console.log(result);
+          console.log(result.confidence);
 
-        console.log(result);
-        console.log(result.confidence);
+          this.parseLines(result.lines);
 
-        let lastDateFound: string = "";
-        // examEvents: {}[] = [];
-        // assignmentEvents: {}[] = [];
+          this.assignmentTableData = new MatTableDataSource(this.assignmentsToReview);
+          this.examTableData = new MatTableDataSource(this.examsToReview);
 
-        result.lines.forEach((line) => {
-          let event = Sherlock.parse(line.text);
-          event.confidence = Math.round(10 * line.confidence) / 10;
+          console.log(this.calendarEvents);
+          console.log(this.examEvents);
+          console.log(this.assignmentEvents);
 
-          if (event.startDate == null && event.endDate == null) {
-            event.probableStartDate = moment(lastDateFound).format('MM/DD/YY');
-            event.needsReview = true;
-            this.calendarEvents.push(event);
-          } else {
-            lastDateFound = event.startDate;
-            event.startDate = moment(event.startDate).format('MM/DD/YY');
-            event.needsReview = false;
-            this.calendarEvents.push(event);
-          }
+          this.recognitionState.status = 'Done';
 
-          for (let word of keywords) {
-            if (event.eventTitle != null && event.eventTitle.toLowerCase().includes(word) && !(this.examEvents.includes(event))) {
-              this.examEvents.push(event);
-              if (event.needsReview) {
-                this.examsToReview.push(event);
-                this.displayExamTable = true;
-              }
-            }
+        })
+        .finally(resultOrError => console.log(resultOrError));
 
-          }
-          if (!(this.assignmentEvents.includes(event)) && !(this.examEvents.includes(event))) {
-            this.assignmentEvents.push(event);
-            if (event.needsReview) {
-              this.assignmentsToReview.push(event);
-              this.displayAssignmentTable = true;
-            }
-          }
-        });
-
-        this.assignmentTableData = new MatTableDataSource(this.assignmentsToReview);
-        this.examTableData = new MatTableDataSource(this.examsToReview);
-
-
-        console.log(this.calendarEvents);
-        console.log(this.examEvents);
-        console.log(this.assignmentEvents);
-
-
-
-        // let allRawDates: string[] = result.text.match(dateRegex);
-
-        // // Establish which date format to use
-        // let format = this.grabDateFormat();
-
-        // // Convert rawDates to moment dates
-        // let allDates = [];
-        // let prevMonths: string[] = [];
-        // for (let rawDate of allRawDates) {
-
-        //   // If rawDate doesn't have three groups, we need to add the group to make it valid for Moment.js
-        //   // If only two groups, we're going to assume it's either MM/DD or DD/MM.
-        //   // If it's MM/YYYY or YYYY/MM it will most likely be in word form, so we're ignoring those for now.
-        //   // So we need to inject a year into the string properly.
-        //   let seperator = /[.,\/ -]/;
-        //   let rawDateSplit = rawDate.split(seperator);
-        //   if(rawDateSplit.length === 2) {
-
-        //     switch(format) {
-        //       case 'MM/DD/YYYY':
-        //       case 'MM.DD.YYYY':
-        //       case 'MM-DD-YYYY':
-        //       case 'MM DD YYYY':
-        //         rawDate = this.addYear(rawDateSplit, prevMonths, rawDate, true, format.match(seperator)[0]);
-        //         break;
-        //       case 'DD/MM/YYYY':
-        //       case 'DD.MM.YYYY':
-        //       case 'DD-MM-YYYY':
-        //       case 'DD MM YYYY':
-        //         rawDate = this.addYear(rawDateSplit, prevMonths, rawDate, false, format.match(seperator)[0]);
-        //         break
-        //     }
-
-        //   }
-
-        //   let date = moment(rawDate, format).format(format);
-        //   allDates.push(date);
-
-        // }
-        // console.log(allDates);
-
-      })
-      .finally(resultOrError => console.log(resultOrError))
   }
+
+
+  parseLines( lines ) {
+
+    let lastDateFound = '';
+    // examEvents: {}[] = [];
+    // assignmentEvents: {}[] = [];
+
+    lines.forEach((line) => {
+      const event = Sherlock.parse(line.text);
+      event.confidence = Math.round(10 * line.confidence) / 10;
+
+      if (event.startDate == null && event.endDate == null) {
+        event.probableStartDate = moment(lastDateFound).format('MM/DD/YY');
+        event.needsReview = true;
+        this.calendarEvents.push(event);
+      } else {
+        lastDateFound = event.startDate;
+        event.startDate = moment(event.startDate).format('MM/DD/YY');
+        event.needsReview = false;
+        this.calendarEvents.push(event);
+      }
+
+      for (const word of keywords) {
+        if (event.eventTitle != null && event.eventTitle.toLowerCase().includes(word) && !(this.examEvents.includes(event))) {
+          this.examEvents.push(event);
+          if (event.needsReview) {
+            this.examsToReview.push(event);
+            this.displayExamTable = true;
+          }
+        }
+
+      }
+      if (!(this.assignmentEvents.includes(event)) && !(this.examEvents.includes(event))) {
+        this.assignmentEvents.push(event);
+        if (event.needsReview) {
+          this.assignmentsToReview.push(event);
+          this.displayAssignmentTable = true;
+        }
+      }
+    });
+
+  }
+
 
 
   /**
@@ -219,22 +177,21 @@ export class UploadAndReviewComponent implements OnInit {
    * @param {boolean} monthFirst
    * @param {string} seperator
    */
-  addYear = (rawDateSplit:string[], prevMonths:string[], rawDate:string, monthFirst:boolean, seperator: string): string => {
+  addYear = (rawDateSplit: string[], prevMonths: string[], rawDate: string, monthFirst: boolean, seperator: string): string => {
 
-    let rawMonth = monthFirst ? rawDateSplit[0]: rawDateSplit[1];
+    const rawMonth = monthFirst ? rawDateSplit[0] : rawDateSplit[1];
 
     // If the month of this date is less than or equal to December, and we've already seen December show up
-    if(Number(rawMonth) <= 12 && prevMonths.includes('12')) {
+    if (Number(rawMonth) <= 12 && prevMonths.includes('12')) {
       rawDate = rawDate.concat(seperator + (new Date().getFullYear() + 1) + '');
-    }
-    else {
+    } else {
       rawDate = rawDate.concat(seperator + new Date().getFullYear() + '');
     }
     prevMonths.push(rawMonth);
 
     return rawDate;
 
-  };
+  }
 
   /**
    * Ascertain the user's locale to determine the Date Format to pass into moment.js
@@ -244,23 +201,23 @@ export class UploadAndReviewComponent implements OnInit {
   grabDateFormat = (): string => {
 
     // Get user locale
-    let locale = window.navigator.language;
+    const locale = window.navigator.language;
 
     // Set locale to moment
     moment.locale(locale);
 
     // Get locale data
-    let localeData = moment.localeData();
-    let format = localeData.longDateFormat('L');
+    const localeData = moment.localeData();
+    const format = localeData.longDateFormat('L');
 
     // Test it
-    let m2 = moment('5-1-2017', format);
+    const m2 = moment('5-1-2017', format);
     console.log(m2.format());
     console.log(m2.format(format) + ' using format: ' + format);
 
     return format;
 
-  };
+  }
 
 
 }
